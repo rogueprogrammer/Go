e5dbebe1f72f10b51c5642338af9108466db80a1
rogueprogrammer <dkhanna@uwaterloo.ca>
2023-01-11 14:37:22 -0800

    Update Channels.go

--- Channels.go
+++ Channels.go
@@ -1,4 +1,9 @@
-import "fmt"
+import (
+    "fmt"
+    "math/rand"
+    "sync"
+    "time"
+)
 // https://www.youtube.com/watch?v=VkGQFFl66X4&list=PLKOhSssLmvQQPFv-QqbRZ10rn3UdBLvVC&index=3
 
 func DoWork() int{
@@ -7,13 +12,16 @@ func DoWork() int{
 }
 
 func main(){
-  dataChan := make(chan int)
+  dataChan := make(chan int) //declare channel
   
-  go func(){
+  go func(){ // declare new go routine
+    wg := sync.WaitGroup{}
     for i:=0; i < 1000; i++{
+      wg.Add(1) // wait for one more go routine
       result := DoWork()
-      dataChan <- result
+      dataChan <- result //insert integer into the channel
     }
+    wg.Wait() // block until all go routines are finished executing
     close(dataChan)
   }()
   
