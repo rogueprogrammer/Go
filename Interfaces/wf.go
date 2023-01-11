3b86a59161b5b9d2bef1d9a398dea8a876105114
rogueprogrammer <dkhanna@uwaterloo.ca>
2023-01-11 14:51:39 -0800

    Create wf.go

---
+++ Interfaces/wf.go
@@ -0,0 +1,27 @@
+package main
+
+type WellsFargo struct {
+  balance int 
+}
+
+func NewWellsFargo() *WellsFargo{
+  return &WellsFargo{
+    balance: 0 
+  }
+}
+
+func (w WellsFargo) GetBalance() int{
+  return w.balance 
+}
+
+func (w WellsFargo) Deposit(amount int) {
+  w.balance += amount 
+}
+
+func (w WellsFargo) Withdraw(amount int) error {
+  if amount > w.balance{
+    return errors.New("insufficient funds")
+  }
+  w.balance -= amount
+  return nil
+}
